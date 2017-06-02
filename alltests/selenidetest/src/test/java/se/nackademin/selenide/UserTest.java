ae69c63a0c1a87e19a417d95782a230a47b15951
Daniel Lundström <Daniel.Lundstrom@yh.nackademin.se>
2017-06-02 20:06:20 +0200

    removed stuff

--- alltests/selenidetest/src/test/java/se/nackademin/selenide/UserTest.java
+++ alltests/selenidetest/src/test/java/se/nackademin/selenide/UserTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package se.nackademin.selenide;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -15,10 +10,6 @@ import org.junit.BeforeClass;
 import se.nackademin.selenidetest.helpers.UserHelper;
 import se.nackademin.selenidetest.model.User;
 
-/**
- *
- * @author daniel
- */
 public class UserTest{
     
     @BeforeClass
